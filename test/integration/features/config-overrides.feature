3a2eba73efbd5f905a0fb4cca686c978213711df
Matt Travi <programmer@travi.org>
2021-06-12 23:04:08 -0500

    test(overrides): defined scenario for existing overrides and none provided
    and wip scenarios for adding provided overrides

--- test/integration/features/config-overrides.feature
+++ test/integration/features/config-overrides.feature
@@ -5,3 +5,23 @@ Feature: Config Overrides
     And additional shareable configs are provided
     When the project is lifted
     Then no overrides are defined in the config file
+
+  @wip
+  Scenario: No existing overrides, overrides provided
+    Given the existing eslint config file has no existing overrides
+    And additional shareable configs, specifying file paths, are provided
+    When the project is lifted
+    Then the expected overrides are defined in the config file
+
+  Scenario: Existing overrides, overrides provided
+    Given the existing eslint config file contains existing overrides
+    And additional shareable configs are provided
+    When the project is lifted
+    Then the expected overrides are defined in the config file
+
+  @wip
+  Scenario: Existing overrides, overrides provided
+    Given the existing eslint config file contains existing overrides
+    And additional shareable configs, specifying file paths, are provided
+    When the project is lifted
+    Then the expected overrides are defined in the config file
--- test/integration/features/step_definitions/config-overrides-steps.js
+++ test/integration/features/step_definitions/config-overrides-steps.js
@@ -2,6 +2,7 @@ import {Given, Then} from '@cucumber/cucumber';
 import {promises as fs} from 'fs';
 import {dump, load} from 'js-yaml';
 import {assert} from 'chai';
+import any from '@travi/any';
 import {eslintConfigScope, pathToYamlConfig} from './eslint-config-steps';
 
 Given('the existing eslint config file has no existing overrides', async function () {
@@ -10,8 +11,34 @@ Given('the existing eslint config file has no existing overrides', async functio
   await fs.writeFile(pathToYamlConfig, dump({extends: eslintConfigScope}));
 });
 
+Given('additional shareable configs, specifying file paths, are provided', async function () {
+  this.additionalShareableConfigs = any.listOf(() => ({...any.simpleObject(), name: any.word(), files: any.string()}));
+});
+
+Given('the existing eslint config file contains existing overrides', async function () {
+  this.eslintConfigScope = eslintConfigScope;
+  this.existingOverrides = any.listOf(() => ({name: any.word(), files: any.string()}));
+
+  await fs.writeFile(pathToYamlConfig, dump({extends: eslintConfigScope, overrides: this.existingOverrides}));
+});
+
 Then('no overrides are defined in the config file', async function () {
   const config = load(await fs.readFile(pathToYamlConfig));
 
   assert.isUndefined(config.overrides);
+});
+
+Then('the expected overrides are defined in the config file', async function () {
+  const config = load(await fs.readFile(pathToYamlConfig));
+  const additionalOverrides = this.additionalShareableConfigs
+    ? this.additionalShareableConfigs.filter(cfg => cfg.files).map(cfg => ({extends: cfg.name, files: cfg.files}))
+    : [];
+
+  assert.deepEqual(
+    config.overrides,
+    [
+      ...this.existingOverrides ? this.existingOverrides : [],
+      ...additionalOverrides
+    ]
+  );
 });
