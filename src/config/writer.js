7587e56ad3aa8d563edc667d42d28d9175630cf7
Matt Travi <programmer@travi.org>
2022-06-03 23:49:53 -0500

    wip(config-file): wrapped the writing fo rhte config to encapsulate the eslint details

---
+++ src/config/writer.js
@@ -0,0 +1,6 @@
+import {fileTypes} from '@form8ion/core';
+import {write} from '@form8ion/config-file';
+
+export default function ({path, config}) {
+  return write({name: 'eslint', format: fileTypes.JSON, path, config});
+}
