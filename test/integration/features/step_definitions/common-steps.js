c2c68a26b82a489881726d2b8eff0b0486bc2c89
Matt Travi <programmer@travi.org>
2022-07-27 15:58:12 -0500

    feat(exports): defined an exports map to make this a true dual-mode package
    BREAKING CHANGE: an exports map has been defined, further restricting access to files in this
    package

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -4,30 +4,12 @@ import stubbedFs from 'mock-fs';
 
 let scaffold, lift;
 const stubbedNodeModules = stubbedFs.load(resolve(__dirname, '..', '..', '..', '..', 'node_modules'));
-const packagePreviewDirectory = '../__package_previews__/eslint';
 
 Before(function () {
   // eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
   ({lift, scaffold} = require('@form8ion/eslint'));
 
-  stubbedFs({
-    node_modules: stubbedNodeModules,
-    [packagePreviewDirectory]: {
-      '@form8ion': {
-        eslint: {
-          node_modules: {
-            ...stubbedNodeModules,
-            '.pnpm': {
-              node_modules: stubbedNodeModules,
-              'ansi-styles@4.3.0': {
-                node_modules: stubbedNodeModules
-              }
-            }
-          }
-        }
-      }
-    }
-  });
+  stubbedFs({node_modules: stubbedNodeModules});
 });
 
 After(function () {
