faa59bcf2818424a574ce79763151a97f3878f55
Matt Travi <programmer@travi.org>
2022-07-27 17:58:51 -0500

    feat(predicate): added a predicate function for determining if the lifter can be used on a project

---
+++ src/predicate-test.js
@@ -0,0 +1,32 @@
+import * as core from '@form8ion/core';
+
+import {assert} from 'chai';
+import any from '@travi/any';
+import sinon from 'sinon';
+
+import configFileExists from './predicate';
+
+suite('predicate', () => {
+  const projectRoot = any.string();
+  let sandbox;
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(core, 'fileExists');
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that `true` is returned when a config file exists', async () => {
+    core.fileExists.withArgs(`${projectRoot}/.eslintrc.yml`).resolves(true);
+
+    assert.isTrue(await configFileExists({projectRoot}));
+  });
+
+  test('that `false` is returned when a config file does not exist', async () => {
+    core.fileExists.resolves(false);
+
+    assert.isFalse(await configFileExists({projectRoot}));
+  });
+});
--- test/integration/features/step_definitions/common-steps.mjs
+++ test/integration/features/step_definitions/common-steps.mjs
@@ -3,13 +3,13 @@ import {fileURLToPath} from 'node:url';
 import {After, Before, When} from '@cucumber/cucumber';
 import stubbedFs from 'mock-fs';
 
-let scaffold, lift;
+let scaffold, lift, test;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const stubbedNodeModules = stubbedFs.load(resolve(__dirname, '..', '..', '..', '..', 'node_modules'));
 
 Before(async function () {
   // eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
-  ({lift, scaffold} = await import('@form8ion/eslint'));
+  ({lift, scaffold, test} = await import('@form8ion/eslint'));
 
   stubbedFs({node_modules: stubbedNodeModules});
 });
@@ -27,9 +27,13 @@ When('the project is scaffolded', async function () {
 });
 
 When('the project is lifted', async function () {
-  this.result = await lift({
-    projectRoot: process.cwd(),
-    configs: this.additionalShareableConfigs,
-    buildDirectory: this.buildDirectory
-  });
+  const projectRoot = process.cwd();
+
+  if (await test({projectRoot})) {
+    this.result = await lift({
+      projectRoot,
+      configs: this.additionalShareableConfigs,
+      buildDirectory: this.buildDirectory
+    });
+  }
 });
